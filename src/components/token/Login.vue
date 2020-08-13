d18881003fd5d01347966d23e082fb2369dc018d
even <even366@qq.com>
2020-08-13 10:49:47 +0800

    添加token解码 获取用户信息

--- src/components/token/Login.vue
+++ src/components/token/Login.vue
@@ -3,16 +3,22 @@
         <input type="text" v-model="loginForm.userName" placeholder="用户名"/>
         <input type="text" v-model="loginForm.password" placeholder="密码"/>
         <button @click="loginSubmit">登录</button>
+        <p></p>
     </div>
     <div>
-        <p>用户信息:</p>
+        <p></p>
+        <p> <span v-if="loginSuccess">登陆成功</span></p>
+        <p>用户ID:{{userInfo.userId}}</p>
+        <p>用户名:{{userInfo.userName}}</p>
+        <p>用户角色:{{userInfo.role}}</p>
     </div>
 </template>
 
 <script>
-    import {reactive} from 'vue';
+    import {reactive,ref} from 'vue';
     import {login} from './../../api/api.js';
-    import {useStore} from 'vuex'
+    import {useStore} from 'vuex';
+    import jwt from 'jwt-decode'
     export default {
         name: "Login",
         setup(){
@@ -22,14 +28,17 @@
                 password:'123'
             });
             const store=useStore();
+            const userInfo=ref({});
+            const loginSuccess=ref(false);
             const loginSubmit= async ()=>{
                 let d = await login();
+                loginSuccess.value=d.code===1?true:false;
                 const userToken = 'Bearer ' + d.data;
                 store.commit('changeLogin',userToken);
-                console.log(store, 'store');
+                userInfo.value = jwt(userToken)  ;
+                
             };
-
-            return {loginForm,loginSubmit}
+            return {loginForm,loginSubmit,userInfo,loginSuccess}
         }
     }
 </script>
