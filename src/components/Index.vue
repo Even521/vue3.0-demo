0585e8d89106052e81af19a2588172abf3c850f3
even <even366@qq.com>
2020-09-03 17:09:48 +0800

    添加element3

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,21 +1,25 @@
 <template>
-    <!-- 菜单 -->
-    <ul>
+  <el-container>
+    <div>
+      <!-- 菜单 -->
+      <ul>
         <li v-for="item in menus">
-            <router-link :to="item.link">{{ item.name }}</router-link>
+          <router-link :to="item.link">{{ item.name }}</router-link>
         </li>
-    </ul>
+      </ul>
+    </div>
+  </el-container>
 </template>
 
 <script>
-    import Menu from '../utils/menu.js'
-    export default {
-        name: 'Index',
-        setup(){
-            const  menus=Menu();
-            return {menus};
-        }
-    }
+import Menu from '../utils/menu.js'
+export default {
+  name: 'Index',
+  setup(){
+    const  menus=Menu();
+    return {menus};
+  }
+}
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 import store from './store'
-
-
+import "element3/lib/theme-chalk/index.css";
+import Element3 from "element3";
 const  app=createApp(App);
 app.use(router);
+app.use(Element3)
 app.use(store);
 app.mount('#app');
